409db383d81b1e6c048e0562dbde78456f1fba93
Leticia-A-Moreira <moreira.leticia@unifesp.br>
2026-06-23 16:52:23 -0300

    Classes NewCommand e DeleteComand que implementam a interface

--- command.cs
+++ command.cs
@@ -1,4 +1,40 @@
+using System;
+using System.Collections.Generic;
+
 public interface ICommand
 {
     void Execute (Object arg);
+}
+
+public class NewCommand : ICommand
+{
+    private Database db;
+    public NewCommand (Database db)
+    {
+        this.db = db;
+    }
+
+    public void Execute(Object arg)
+    {
+        Object[] args = (Object[])arg;
+        int id = (int)args[0];
+        string nome = (string)args[1];
+
+        db.Inserir(new Pessoa(id, nome));
+    }
+}
+
+public class DeleteCommand : ICommand
+{
+    private Database db;
+    public DeleteCommand (Database db)
+    {
+        this.db = db;
+    }
+
+    public void Execute(Object arg)
+    {
+        int id = (int)arg;
+        db.Deletar(id);
+    }
 }
