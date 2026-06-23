f6725e254051cdbbc1774225b3d980fe5fa8e167
Leticia-A-Moreira <moreira.leticia@unifesp.br>
2026-06-23 19:46:37 -0300

    Classe Pessoa

--- command.cs
+++ command.cs
@@ -1,6 +1,36 @@
 using System;
 using System.Collections.Generic;
 
+public class Pessoa
+{
+    public int Id
+    {
+        get; 
+        set;
+    }
+    public string Nome
+    {
+        get; 
+        set;
+    }
+
+    // CONSTRUTOR
+    // quando o seu NewCommand for executado, 
+    // ele vai fazer uma chamada do tipo new Pessoa(1, "Fulano")
+    public Pessoa (int id, string nome)
+    {
+        Id = id;
+        Nome = nome;
+    }
+    // => - formata a informacao e devolve a string
+    // deixa a info pronta para ser impressa
+    public override string ToString() => $"ID: {Id} --> {Nome}";
+}
+
+
+
+
+
 public interface ICommand
 {
     void Execute (Object arg);
